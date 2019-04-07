ac519cdc87042ed2eabc9e3eecc417f7b736eb7c
LodesJacob <46973657+LodesJacob@users.noreply.github.com>
2019-04-07 12:26:49 -0400

    Challenge 3

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject hazard;
+    public GameObject[] hazards;
     public Vector3 spawnValues;
     public int hazardCount;
     public float spawnWait;
@@ -16,6 +16,7 @@ public class GameController : MonoBehaviour
     public Text scoreText;
     public Text restartText;
     public Text gameOverText;
+    public Text creditText;
 
     private bool gameOver;
     private bool restart;
@@ -28,6 +29,7 @@ public class GameController : MonoBehaviour
         StartCoroutine(SpawnWaves());
         restartText.text = "";
         gameOverText.text = "";
+        creditText.text = "";
     }
 
     private void Update()
@@ -38,7 +40,7 @@ public class GameController : MonoBehaviour
         }
         if (restart)
         {
-            if (Input.GetKeyDown (KeyCode.R))
+            if ( Input.GetKeyDown (KeyCode.Space))
             {
                 SceneManager.LoadScene(SceneManager.GetActiveScene().name);
             }
@@ -52,36 +54,55 @@ public class GameController : MonoBehaviour
         {
             for (int i = 0; i < hazardCount; i++)
             {
+                GameObject hazard = hazards[Random.Range(0, hazards.Length)];
                 Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
                 Quaternion spawnRotation = Quaternion.identity;
                 Instantiate(hazard, spawnPosition, spawnRotation);
                 yield return new WaitForSeconds(spawnWait);
+
+                if (gameOver)
+                {
+                    break;
+                }
             }
             yield return new WaitForSeconds(waveWait);
 
             if (gameOver)
             {
-                restartText.text = "Press 'R' to Restart";
+                restartText.text = "Press Space to Restart";
                 restart = true;
                 break;
             }
         }
     }
 
+    private void setCredit()
+    {
+        creditText.text = "Made by Jacob Lodes";
+    }
+
     public void AddScore(int newScoreValue)
     {
         score += newScoreValue;
         UpdateScore();
+
+        if (score >= 100)
+        {
+            gameOverText.text = "You Win!";
+            setCredit();
+            gameOver = true;
+        }
     }
 
     void UpdateScore()
     {
-        scoreText.text = "Score: " + score;
+        scoreText.text = "Points: " + score;
     }
 
     public void GameOver()
     {
         gameOverText.text = "Game Over!";
-        gameOver = true;        
+        setCredit();
+        gameOver = true;
     }
 }
